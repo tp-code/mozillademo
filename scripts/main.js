c28851434dbda1a510888599867570fa46491ae8
tp-code <41316251+tp-code@users.noreply.github.com>
2018-07-16 21:28:45 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'img/firefox-logo-800.png') {
+      myImage.setAttribute ('src','img/firefox-logo-2-800.png');
+    } else {
+      myImage.setAttribute ('src','img/firefox-logo-800.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
